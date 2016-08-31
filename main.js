3dd84f09bdeca72fae2111c400655793088fdb23
euwbah <euwbah@gmail.com>
2016-08-31 13:22:10 +0800

    Logging

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ function _generateMeme(first, second) {
   var bouncedIndex = 0;
 
   for (var count = 0; count < first.length < second.length ? first.length : second.length; count++) {
+    console.log("Identical Index Count: " + count);
     if (first[count] == second[count])
       bouncedIndex = count;
     else
@@ -13,10 +14,12 @@ function _generateMeme(first, second) {
 
   for (i = first.length; i > bouncedIndex; i--) {
     meme += first.substring(0, i) + "\n";
+    console.log("First Index Count: " + i);
   }
 
   for (i = bouncedIndex + 1; i <= second.length; i++) {
     meme += second.substring(0, i) + "\n";
+    console.log("Second Index Count: " + i);
   }
 
   return meme;
