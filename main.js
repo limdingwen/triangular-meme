9a576997983d0b6b16b7c217c2212391975da37c
Ignis Incendio <limdingwen@gmail.com>
2016-08-31 14:23:59 +0800

    Cleaned up JS and removed debug code.

--- main.js
+++ main.js
@@ -5,8 +5,7 @@ function _generateMeme(first, second) {
   var bouncedIndex = 0;
 
   if (first != second) {
-    for (var count = 0; count < first.length < second.length ? first.length : second.length; count++) {
-      console.log("Identical Index Count: " + count);
+    for (var count = 0; count < (first.length < second.length ? first.length : second.length); count++) {
       if (first[count] == second[count])
         bouncedIndex = count;
       else
@@ -15,12 +14,10 @@ function _generateMeme(first, second) {
 
     for (i = first.length; i > bouncedIndex; i--) {
       meme += first.substring(0, i) + "\n";
-      console.log("First Index Count: " + i);
     }
 
     for (i = bouncedIndex + 2; i <= second.length; i++) {
       meme += second.substring(0, i) + "\n";
-      console.log("Second Index Count: " + i);
     }
   }
 
