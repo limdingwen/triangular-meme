7eddeb922662e0fff3242519872d60a91b885f78
euwbah <euwbah@gmail.com>
2016-08-31 13:26:01 +0800

    Fix again?

--- main.js
+++ main.js
@@ -12,15 +12,18 @@ function _generateMeme(first, second) {
       break;
   }
 
-  for (i = first.length; i > bouncedIndex + 1; i--) {
-    meme += first.substring(0, i) + "\n";
-    console.log("First Index Count: " + i);
-  }
+  if (first != second) {
+    for (i = first.length; i > bouncedIndex + 1; i--) {
+      meme += first.substring(0, i) + "\n";
+      console.log("First Index Count: " + i);
+    }
 
-  for (i = bouncedIndex + 1; i <= second.length; i++) {
-    meme += second.substring(0, i) + "\n";
-    console.log("Second Index Count: " + i);
-  }
+    for (i = bouncedIndex + 2; i <= second.length; i++) {
+      meme += second.substring(0, i) + "\n";
+      console.log("Second Index Count: " + i);
+    }
+  } else
+    return first;
 
   return meme;
 }
